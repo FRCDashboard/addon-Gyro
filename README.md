9e170ed279be1d1f0126a1df9339906481d0dbab
ErikBoesen <me@erikboesen.com>
2017-05-25 13:19:02 -0400

    Code and README rewrite for compatibility with new addon format

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Gyroscope Widget addon for FRC Dashboard
-Live updating, minimal gyro widget. Supports resetting to zero on click for calibration.
+Live updating, bare-bones gyro widget. Supports resetting to zero on click for calibration.
 
 ## Installation
 1. Copy the contents of `gyro.html` to wherever in the dashboard you desire.
-2. Copy the contents of `gyro.css` to `style.css` in the dashboard. Of course, you can put it anywhere else you would normally put CSS.
-3. Copy the sections of `gyro.js` to where the comments above each section say they should go.
+2. Copy `gyro.js` into the `components` directory of your FRCDashboard system.
+3. Link the script from `index.html`, for example:
+
+```html
+<script src="components/gyro.js"></script>
+```
+
+4. Copy `gyro.css` to the `css` directory in your FRCDashboard system.
+5. Link the CSS from `index.html`, for example:
+
+```html
+<link rel="stylesheet" href="css/gyro.css">
+```
+
+6. If necessary, change the keys of the NetworkTables values used in `gyro.js` to the keys your robot code uses.
+
+Alternatively, you could just copy all the JavaScipt code into `ui.js` or other JavaScript script, but that's a bit tougher to keep organized.
--- gyro.js
+++ gyro.js
@@ -1,21 +1,14 @@
-// This should be added inside the definition of the 'ui' object at the starting of ui.js.
+// Set a global alias for the select box.
+ui.gyro = {
+    container: document.getElementById('gyro'),
+    val: 0,
+    offset: 0,
+    visualVal: 0,
+    arm: document.getElementById('gyro-arm'),
+    number: document.getElementById('gyro-number')
+};
 
-    gyro: {
-        container: document.getElementById('gyro'),
-        val: 0,
-        offset: 0,
-        visualVal: 0,
-        arm: document.getElementById('gyro-arm'),
-        number: document.getElementById('gyro-number')
-    },
-
-// End section
-
-
-
-// Copy this portion of the code into the large switch statement in the onValueChanged function
-
-case '/SmartDashboard/Drive/NavX | Yaw': // Gyro rotation
+NetworkTables.addKeyListener('/SmartDashboard/drive/navx/yaw', (key, value) => {
     ui.gyro.val = value;
     ui.gyro.visualVal = Math.floor(ui.gyro.val - ui.gyro.offset);
     if (ui.gyro.visualVal < 0) { // Corrects for negative values
@@ -23,21 +16,15 @@ case '/SmartDashboard/Drive/NavX | Yaw': // Gyro rotation
     }
     ui.gyro.arm.style.transform = ('rotate(' + ui.gyro.visualVal + 'deg)');
     ui.gyro.number.innerHTML = ui.gyro.visualVal + 'º';
-    break;
+});
 
-// End section
-
-
-
-// Add this at the bottom of ui.js with the other listeners.
-// You can skip this part if you don't want the gyro to have reset on click functionality.
+// You can remove this part if you don't want the gyro to have reset on click functionality.
 
 // Reset gyro value to 0 on click
 ui.gyro.container.onclick = function() {
 	// Store previous gyro val, will now be subtracted from val for callibration
 	ui.gyro.offset = ui.gyro.val;
-	// Trigger the gyro to recalculate value.
-	onValueChanged('/SmartDashboard/Drive/NavX | Yaw', ui.gyro.val);
+    // Show the gyro as zeroed out.
+    // TODO: This feels hacky, fix it.
+    ui.gyro.arm.style.transform = 'rotate(0)';
 };
-
-// End section
